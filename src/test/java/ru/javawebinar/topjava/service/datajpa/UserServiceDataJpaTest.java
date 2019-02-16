ebd14100fb1b3b0c57a118c45da1b1358574abb9
Alexander Alexandrov <resident0007@gmail.com>
2019-02-17 02:23:36 +0300

    HW5. Code review # 2

--- src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
@@ -3,12 +3,17 @@ package ru.javawebinar.topjava.service.datajpa;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.Collections;
+import java.util.Date;
+
 import static ru.javawebinar.topjava.MealTestData.MEALS;
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 import static ru.javawebinar.topjava.UserTestData.assertMatch;
@@ -30,6 +35,17 @@ public class UserServiceDataJpaTest extends UserServiceTest {
     @Test(expected = NotFoundException.class)
     public void getUserWithMealNotFoundTest() {
         User user = service.getWithMeals(123);
+    }
+
+    @Test
+    public void getUserWithoutMealTest() {
+        User newUser = new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Collections.singleton(Role.ROLE_USER));
+        User created = service.create(newUser);
+        newUser.setId(created.getId());
+        int id = created.getId();
+        assertMatch(service.getAll(), ADMIN, newUser, USER);
+        User user = service.getWithMeals(id);
+        assertMatch(user, newUser);
     }
 
 }
